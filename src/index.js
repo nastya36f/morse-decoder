4cb28f13bcd47129c8d6ff1ef9af98129e6e05b1
Nastya Filippova <nastya.filippova87@gmail.com>
2022-07-15 09:39:14 +0300

    fix: create function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let string = '';
+    for ( let i = 0; i < expr.length; ) {
+        let ten = expr.slice(i, i + 10);    //цикл разбивает на десятки
+        let morse = '';
+        if (ten === '**********') {
+            string += ' ';
+            
+        }else {
+            for (let j = 10; j > 0;) {                      // цикл перебирает десяток и превращает в точки-тире
+                let letter = ten.slice(j-2, j);
+                if(letter === '10') {
+                    morse = '.' + morse;
+                }else {
+                    if(letter === '11'){
+                        morse = '-' + morse;
+                    }else {
+                        break;
+                    }                  
+                }
+                j -= 2;
+            }
+            string += MORSE_TABLE[morse];
+        }
+        i += 10;
+    }
+
+    return string;
 }
 
 module.exports = {
